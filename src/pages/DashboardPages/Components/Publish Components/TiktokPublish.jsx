e4de2f0e28995561ce0c46a66d7425ff235186f2
Suhaib Alghamdi <suhaib.ag.49@gmail.com>
2025-04-29 18:35:09 +0300

    Revert "fixes to the publish page on tiktok"

--- src/pages/DashboardPages/Components/Publish Components/TiktokPublish.jsx
+++ src/pages/DashboardPages/Components/Publish Components/TiktokPublish.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import TikTokPost from "../Posts/TiktokPost";
 import TiktokPopUp from "./TiktokPopUp";
 import FakeTiktokPost from "../Posts/FakeTiktokPost";
-import FakeTikTokPost from "../Posts/FakeTiktokPost";
 const TiktokPublish = ({postType, posts}) =>{
 
     const [queue, setQueue] = useState([])
@@ -35,49 +34,48 @@ const TiktokPublish = ({postType, posts}) =>{
     },[postType, queue])
 
     return(
-
+      
+      
       <div className="flex-col w-1/2">
-          {posting?
-              <div>
-                  <TiktokPopUp 
-                  setPosting ={setPosting}/>
-              </div>
-              :
-              <div>
-                   <div className='bg-slate-50 ml-24'>
-                      {<button  
-                          onClick={() => setPosting(true)}
-                          className='bg-blue-500 w-20 h-10 justify-center items-center text-white text-2xl hover:bg-blue-200 transition m-4 '>
-                              +
-                      </button>}
-                  </div>
-                          
-              </div>
-          }
-  
-  
-        <div className="flex flex-col justify-start items-center mt-24 shadow-md bg-white rounded-lg  space-y-4 p-4 ">  
-        {mapper.map((post, index) => (
-          <div key={index} className="flex-row justify-center h-full w-full mb-5">
-  
-              
-  
-              {postType != "Posted"?
-                  <div className="flex justify-center ">
-                      <FakeTikTokPost post={post}/>
-                      <button className="bg-red-600 text-white rounded-xl mt-3 p-3 flex justify-center items-center hover:bg-blue-400">Delete</button>                    
-                      <button className="bg-blue-500 text-white rounded-xl mt-3 p-3 flex justify-center items-center hover:bg-blue-400"> Post Now</button>
-                  </div>
-              :
-                  <div><FakeTikTokPost post={post}/></div>
-              }
-              
+        {posting?
+            <div>
+                <TiktokPopUp 
+                setPosting ={setPosting}/>
             </div>
-        ))}
-  
-      </div>
-  
-      </div>
+            :
+            <div>
+                 <div className='bg-slate-50 ml-24'>
+                    {<button  
+                        onClick={() => setPosting(true)}
+                        className='bg-blue-500 w-20 h-10 justify-center items-center text-white text-2xl hover:bg-blue-200 transition m-4 '>
+                            +
+                    </button>}
+                </div>
+                        
+            </div>
+        }
+
+
+      <div className="flex flex-col justify-start items-center mt-24 shadow-md bg-white rounded-lg  space-y-4 p-4 ">
+
+      {mapper.map((post, index) => (
+        <div key={index} className="flex-row justify-center h-full w-full mb-5">
+            <div className="mb-4 text-sm"></div>
+            <FakeTiktokPost post={post}/>
+            {postType != "Posted"?
+                <div className="flex justify-center">
+                    <button className="bg-red-600 text-white rounded-xl mt-3 p-3 flex justify-center items-center hover:bg-blue-400">Delete</button>                    
+                    <button className="bg-blue-500 text-white rounded-xl mt-3 p-3 flex justify-center items-center hover:bg-blue-400"> Post Now</button>
+                </div>
+            :
+                <div></div>
+            }
+            
+          </div>
+      ))}
+
+    </div>
+    </div>
     )
 
 }
